3448d5ce43819c84d922e06e7c64d516f7d0dab2
Metin Ahmad <metinmayi@gmail.com>
2022-06-07 21:56:22 +0200

    Added model for animals

---
+++ src/models/Animal.ts
@@ -0,0 +1,12 @@
+export interface Animal {
+  id: number;
+  imageUrl: string;
+  isfed: boolean;
+  lastFed: string;
+  latinName: string;
+  longDescription: string;
+  medicine: string;
+  name: string;
+  shortDescription: string;
+  yearOfBirth: number;
+}
