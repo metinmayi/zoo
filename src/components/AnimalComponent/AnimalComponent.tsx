8edf2a842f14e1e4d8b0af458f233da8cb2c0790
Metin Ahmad <metinmayi@gmail.com>
2022-06-11 23:05:33 +0200

    Error handling for images

--- src/components/AnimalComponent/AnimalComponent.tsx
+++ src/components/AnimalComponent/AnimalComponent.tsx
@@ -1,30 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Animal } from "../../models/Animal";
 import { ZooButton } from "../ZooButton";
-import { Description } from "./description";
 import { Img } from "./img";
 import { Title } from "./title";
 import { Wrapper } from "./wrapper";
 
 type Props = {
   animal: Animal;
+  large?: boolean;
 };
-const AnimalComponent: React.FC<Props> = ({ animal }) => {
+const AnimalComponent: React.FC<Props> = ({ animal, large }) => {
   // Redirect to animal function
   const navigate = useNavigate();
-  const redirect: Function = (id: number) => {
-    navigate(`/${id}`);
+  const redirect: Function = (href?: number) => {
+    href ? navigate(`/${href}`) : navigate(-1);
   };
 
+  const [bgImg, setBgImg] = useState(animal.imageUrl);
+
+  const backupImage: string =
+    "https://thumbs.dreamstime.com/b/unavailable-stamp-round-vintage-grunge-label-sign-186956132.jpg";
+
   return (
-    <Wrapper>
-      <Img imgURL={animal.imageUrl}></Img>
+    <Wrapper large={large}>
+      <Img
+        src={bgImg}
+        large={large}
+        onError={() => setBgImg(backupImage)}></Img>
       <Title>{animal.name}</Title>
-      <Description>{animal.shortDescription}</Description>
-      <ZooButton handleClick={() => redirect(animal.id)}>
-        Visit Animal
-      </ZooButton>
+      <p>{large ? animal.longDescription : animal.shortDescription}</p>
+      {large ? (
+        <ZooButton handleClick={() => redirect()}>Return</ZooButton>
+      ) : (
+        <ZooButton handleClick={() => redirect(animal.id)}>
+          Visit Animal
+        </ZooButton>
+      )}
     </Wrapper>
   );
 };
