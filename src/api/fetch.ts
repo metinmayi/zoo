db04452e0ed70125d6aa9f6acf3861766ff513e1
Metin Ahmad <metinmayi@gmail.com>
2022-06-11 23:05:14 +0200

    Added function to sync IDs with Indexes

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,7 +1,7 @@
 import { Animal } from "../models/Animal";
 
 // In a real project this would be a proper fetch.
-const getStoredAnimals = () => {
+const getStoredAnimals = (): Animal[] => {
   const storedAnimals: string = localStorage.getItem("zoo") || "[]";
   const animals: Animal[] = JSON.parse(storedAnimals);
   return animals;
@@ -10,7 +10,13 @@ const getStoredAnimals = () => {
 const fetchNewAnimals = async () => {
   const result = await fetch("https://animals.azurewebsites.net/api/animals");
   const data = await result.json();
+  syncIdWithIndex(data);
   localStorage.setItem("zoo", JSON.stringify(data));
+};
+
+const syncIdWithIndex = (animals: Animal[]): Animal[] => {
+  animals.forEach((animal, index) => (animal.id = index));
+  return animals;
 };
 
 export { getStoredAnimals, fetchNewAnimals };
